08226c54e5d0e4369d21a5ba37de5c9d9719a8bd
Oren Shalev <orenico4@gmail.com>
2023-01-08 13:15:42 +0200

    adhustments for serverless framewotk on amazon lambda

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,67 @@
 import { ApolloServer } from '@apollo/server';
-import { readFileSync } from 'fs';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { CryptoCompareAPI } from "./DataSources/CryptoCompareAPI.js";
 import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = readFileSync('./src/schema.graphql', { encoding: 'utf-8' });
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  type User {
+    id: ID!
+    name: String
+  }
+
+   "This Book type defines the queryable fields for every book in our data source."
+  type Book {
+    """
+    title will be used for the book title
+    """
+    title: String
+    author: String
+  }
+
+  input BookInfo {
+    title: String
+    author: String
+  }
+
+  type Coin {
+    id: String
+    symbol: String
+    coinName: String
+    imageUrl: String
+  }
+
+  input HistoryParams {
+    fsym: String
+    tsym: String
+    limit: Int
+    aggregate: Int
+  }
+
+  type Point {
+    x: Float
+    y: Float
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+    numberSix: Int!
+    numberSeven: Int!
+    user(id: ID!): User
+    listCoins(page: Int!): [Coin]
+    listHistory(params: HistoryParams): [Point]
+  }
+
+  type Mutation {
+    addBook(title: String, author: String): Book
+  }
+`;
 const books = [
     {
         title: 'The Awakening',
@@ -64,8 +121,19 @@ const server = new ApolloServer({
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-// const port = Number(process.env.port)  || 3000;
-// const { url } = await startStandaloneServer(server, {
+const port = Number(process.env.port) || 3000;
+const { url } = await startStandaloneServer(server, {
+    context: async () => ({
+        // We create new instances of our data sources with each request,
+        // passing in our server's cache.
+        dataSources: {
+            cryptoCompareApi: new CryptoCompareAPI(),
+        }
+    }),
+    //  listen: { port: port }
+});
+console.log(`🚀  Server ready at: ${url}`);
+// export const graphqlHandler = startServerAndCreateLambdaHandler(server, {
 //   context: async () => ({
 //      // We create new instances of our data sources with each request,
 //      // passing in our server's cache.
@@ -73,6 +141,13 @@ const server = new ApolloServer({
 //       cryptoCompareApi: new CryptoCompareAPI(),
 //      }
 //    }),
-//   //  listen: { port: port }
-// });
-export const graphqlHandler = startServerAndCreateLambdaHandler(server);
+// }); 
+export const graphqlHandler = startServerAndCreateLambdaHandler(server, {
+    context: async () => ({
+        // We create new instances of our data sources with each request,
+        // passing in our server's cache.
+        dataSources: {
+            cryptoCompareApi: new CryptoCompareAPI(),
+        }
+    }),
+});
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,72 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import {CryptoCompareAPI} from "./DataSources/CryptoCompareAPI.js";
 import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda'; 
+import somePath from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-const typeDefs = readFileSync('./src/schema.graphql', { encoding: 'utf-8' });
+
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  type User {
+    id: ID!
+    name: String
+  }
+
+   "This Book type defines the queryable fields for every book in our data source."
+  type Book {
+    """
+    title will be used for the book title
+    """
+    title: String
+    author: String
+  }
+
+  input BookInfo {
+    title: String
+    author: String
+  }
+
+  type Coin {
+    id: String
+    symbol: String
+    coinName: String
+    imageUrl: String
+  }
+
+  input HistoryParams {
+    fsym: String
+    tsym: String
+    limit: Int
+    aggregate: Int
+  }
+
+  type Point {
+    x: Float
+    y: Float
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+    numberSix: Int!
+    numberSeven: Int!
+    user(id: ID!): User
+    listCoins(page: Int!): [Coin]
+    listHistory(params: HistoryParams): [Point]
+  }
+
+  type Mutation {
+    addBook(title: String, author: String): Book
+  }
+`;
 
 const books = [
     {
