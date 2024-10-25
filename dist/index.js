72f03bf7be0e1ca2913abb6d3fddd0d29a9280fd
Oren Shalev <orenico4@gmail.com>
2024-10-25 10:42:11 +0300

    commit for push

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,17 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import { CryptoCompareAPI } from "./DataSources/CryptoCompareAPI.js";
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import Express, { json } from 'express';
+import cors from 'cors';
+import http from 'http';
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
+const app = Express();
+const httpServer = http.createServer(app);
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  type User {
-    id: ID!
-    name: String
-  }
-
-   "This Book type defines the queryable fields for every book in our data source."
-  type Book {
-    """
-    title will be used for the book title
-    """
-    title: String
-    author: String
-  }
-
-  input BookInfo {
-    title: String
-    author: String
-  }
-
   type Coin {
     id: String
     symbol: String
@@ -49,48 +35,16 @@ const typeDefs = `#graphql
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    # books: [Book]
-    # numberSix: Int!
-    # numberSeven: Int!
-    # user(id: ID!): User
     listCoins(page: Int!): [Coin]
     listHistory(params: HistoryParams): [Point]
   }
 
-  type Mutation {
-    addBook(title: String, author: String): Book
-  }
+  # type Mutation {}
 `;
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin',
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster',
-    },
-];
-const users = [
-    {
-        id: '1',
-        name: 'Elizabeth Bennet',
-    },
-    {
-        id: '2',
-        name: 'Fitzwilliam Darcy',
-    },
-];
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-        // books: () => books,
-        // numberSix: () => 6,
-        // numberSeven: () => 7,
-        // user(parent, args, context, info) {
-        //   return users.find((user) => user.id === args.id);
-        // },
         listCoins(_, { page }, { dataSources }) {
             return dataSources.cryptoCompareApi.getCoinsList(page);
         },
@@ -99,39 +53,48 @@ const resolvers = {
             return dataSources.cryptoCompareApi.fetchHistory(fsym, tsym, limit, aggregate);
         }
     },
-    Mutation: {
-        addBook: (_, args) => {
-            const book = {
-                title: args.title,
-                author: args.author
-            };
-            books.push(book);
-            return book;
-        }
-    }
+    // Mutation: {
+    // } 
 };
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const port = Number(process.env.port) || 3000;
-const { url } = await startStandaloneServer(server, {
-    context: async () => ({
-        // We create new instances of our data sources with each request,
-        // passing in our server's cache.
+await server.start();
+app.use('/', cors(), 
+// 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+json({ limit: '50mb' }), 
+// expressMiddleware accepts the same arguments:
+// an Apollo Server instance and optional configuration options
+expressMiddleware(server, {
+    context: async ({ req }) => ({
+        token: "",
         dataSources: {
             cryptoCompareApi: new CryptoCompareAPI(),
         }
     }),
-    listen: { port: port }
-});
-console.log(`🚀  Server ready at: ${url}`);
+}));
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/`);
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+// const port = Number(process.env.port)  || 3000;
+// const { url } = await startStandaloneServer(server, {
+//   context: async () => ({
+//      // We create new instances of our data sources with each request,
+//      // passing in our server's cache.
+//      dataSources: {
+//       cryptoCompareApi: new CryptoCompareAPI(),
+//      }
+//    }),
+//    listen: { port: port }
+// });
+// console.log(`🚀  Server ready at: ${url}`);
 // export const graphqlHandler = startServerAndCreateLambdaHandler(server, {
 //       context: async () => ({
 //      // We create new instances of our data sources with each request,
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,19 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import {CryptoCompareAPI} from "./DataSources/CryptoCompareAPI.js";
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda'; 
-import somePath from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
+import  Express,{json}  from 'express';
+import  bodyParser from 'body-parser'
+import cors from 'cors';
+import http from 'http';
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
-
+const app = Express()
+const httpServer = http.createServer(app);
 
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
@@ -41,7 +45,7 @@ const typeDefs = `#graphql
     listHistory(params: HistoryParams): [Point]
   }
 
-  type Mutation {}
+  # type Mutation {}
 `;
 
   // Resolvers define how to fetch the types defined in your schema.
@@ -56,9 +60,9 @@ const resolvers = {
         return dataSources.cryptoCompareApi.fetchHistory(fsym, tsym, limit, aggregate);
       }
     },
-    Mutation: {
+    // Mutation: {
 
-    } 
+    // } 
   };
 
 
@@ -73,25 +77,53 @@ interface ContextValue {
 const server = new ApolloServer<ContextValue>({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   
+
+  await server.start();
+
+  app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+    json({ limit: '50mb' }),
+    // expressMiddleware accepts the same arguments:
+    // an Apollo Server instance and optional configuration options
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ 
+        token: "",
+        dataSources: {
+          cryptoCompareApi: new CryptoCompareAPI(),
+        }
+      }),
+    }),
+  );
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/`);
+  
+
   // Passing an ApolloServer instance to the `startStandaloneServer` function:
   //  1. creates an Express app
   //  2. installs your ApolloServer instance as middleware
   //  3. prepares your app to handle incoming requests
-  const port = Number(process.env.port)  || 3000;
+  // const port = Number(process.env.port)  || 3000;
 
-  const { url } = await startStandaloneServer(server, {
-    context: async () => ({
-       // We create new instances of our data sources with each request,
-       // passing in our server's cache.
-       dataSources: {
-        cryptoCompareApi: new CryptoCompareAPI(),
-       }
-     }),
-     listen: { port: port }
-  });
-  console.log(`🚀  Server ready at: ${url}`);
+  // const { url } = await startStandaloneServer(server, {
+  //   context: async () => ({
+  //      // We create new instances of our data sources with each request,
+  //      // passing in our server's cache.
+  //      dataSources: {
+  //       cryptoCompareApi: new CryptoCompareAPI(),
+  //      }
+  //    }),
+  //    listen: { port: port }
+  // });
+  // console.log(`🚀  Server ready at: ${url}`);
+
+
+
 
 
   // export const graphqlHandler = startServerAndCreateLambdaHandler(server, {
